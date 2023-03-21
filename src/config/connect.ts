6c163cd5848a3b269d3307407f556b4e749e8cb7
Stephen Korankye <53407122+stephenkorankye@users.noreply.github.com>
2023-03-21 19:02:00 +0000

    Re-constructing PostgreSQL URL

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -1,5 +1,5 @@
-const { DB_PASSWORD } = process.env ; 
+const { DB_PASSWORD , DB_URI , DB_PORT } = process.env ; 
 
-const connKey = `postgresql://doadmin:${DB_PASSWORD}@db-postgresql-fra1-38922-do-user-13073615-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=no-verify`;
+const connKey = `postgresql://doadmin:${DB_PASSWORD}@${DB_URI}:${DB_PORT}/defaultdb?sslmode=no-verify`;
 
 export default connKey;
